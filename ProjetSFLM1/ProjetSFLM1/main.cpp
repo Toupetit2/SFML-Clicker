cc8c8e780bece3aa2ef5832585f111d557a636f6
Toupetit2 <theo.doublet@yahoo.com>
2024-11-21 12:15:50 +0100

    init

---
+++ ProjetSFLM1/ProjetSFLM1/main.cpp
@@ -0,0 +1,126 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int x = 300;
+int y = 300;
+
+
+
+vector<vector<int>> tilemap = {
+    // Lignes vides (le ciel)
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+
+
+
+sf::Vector2f camPosition(0, 0);
+
+sf::Vector2f characterPosition(2, 2);
+
+float characterSize = 0.8f;
+
+
+void drawTerrain(sf::RenderWindow winow)
+{
+    
+}
+
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(768, 720), "Gestion des Entrées");
+    window.setFramerateLimit(60);
+
+    
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Left)
+                {
+                    std::cout << "GAUCHE" << std::endl;
+                    characterPosition.x -= 0.2f;
+                }
+                if (event.key.code == sf::Keyboard::Right) {
+                    std::cout << "DROITE" << std::endl;
+                    characterPosition.x += 0.2f;
+                }
+                if (event.key.code == sf::Keyboard::Up) {
+                    std::cout << "HAUT" << std::endl;
+                    characterPosition.y -= 1;
+                }
+                if (event.key.code == sf::Keyboard::Down) {
+                    std::cout << "BAS" << std::endl;
+                }
+                if (event.key.code == sf::Keyboard::Escape)
+                    window.close(); // Fermer avec Échap
+            }
+        }
+
+        window.clear();
+
+        //fonction1
+        int nbline = 0;
+        for (vector<int> line : tilemap)
+        {
+            int nbtile = 0;
+            for (int tile : line) {
+                sf::RectangleShape shape(sf::Vector2f(100.0f, 100.0f));
+                
+                if (tile == 0) {
+                    shape.setFillColor(sf::Color::Black);
+                }
+                if (tile == 1) {
+                    shape.setFillColor(sf::Color(94, 52, 34));
+                }
+                
+                shape.setPosition(nbtile * 48, nbline * 48);
+                nbtile++;
+
+                window.draw(shape);
+    
+            }
+            nbline++;
+        }
+        //fin de fonction1
+
+        //gravité personnage
+        if (tilemap[int(characterPosition.y + characterSize)][characterPosition.x] == 0)
+        {
+            characterPosition.y += 0.05f;
+        }
+
+        //affichage personnage
+        sf::RectangleShape shape(sf::Vector2f(characterSize * 48, characterSize * 48));
+        shape.setFillColor(sf::Color::Blue);
+        shape.setPosition(characterPosition.x*48, characterPosition.y * 48);
+        window.draw(shape);
+
+        window.display();
+    }
+
+    return 0;
+}
